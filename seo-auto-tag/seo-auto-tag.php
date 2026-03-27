a259e618402877606f77956d2f7cd7d320e30900
Tim Hetrick <timhetrick@gmail.com>
2026-03-27 14:04:46 -0500

    Add files via upload
    version 0.1

--- seo-auto-tag/seo-auto-tag.php
+++ seo-auto-tag/seo-auto-tag.php
@@ -1,8 +1,8 @@
 <?php
 namespace Grav\Plugin;
 
-use Composer\Autoload\ClassLoader;
 use Grav\Common\Plugin;
+use RocketTheme\Toolbox\Event\Event;
 
 /**
  * Class SeoAutoTagPlugin
@@ -12,33 +12,12 @@ class SeoAutoTagPlugin extends Plugin
 {
     /**
      * @return array
-     *
-     * The getSubscribedEvents() gives the core a list of events
-     *     that the plugin wants to listen to. The key of each
-     *     array section is the event that the plugin listens to
-     *     and the value (in the form of an array) contains the
-     *     callable (or function) as well as the priority. The
-     *     higher the number the higher the priority.
      */
     public static function getSubscribedEvents(): array
     {
         return [
-            'onPluginsInitialized' => [
-                // Uncomment following line when plugin requires Grav < 1.7
-                // ['autoload', 100000],
-                ['onPluginsInitialized', 0]
-            ]
+            'onPluginsInitialized' => ['onPluginsInitialized', 0]
         ];
-    }
-
-    /**
-     * Composer autoload
-     *
-     * @return ClassLoader
-     */
-    public function autoload(): ClassLoader
-    {
-        return require __DIR__ . '/vendor/autoload.php';
     }
 
     /**
@@ -48,12 +27,63 @@ class SeoAutoTagPlugin extends Plugin
     {
         // Don't proceed if we are in the admin plugin
         if ($this->isAdmin()) {
+            $this->enable([
+                'onBlueprintCreated' => ['onBlueprintCreated', 0]
+            ]);
             return;
         }
 
         // Enable the main events we are interested in
         $this->enable([
-            // Put your main events here
+            'onTwigTemplatePaths' => ['onTwigTemplatePaths', 0],
+            'onPageInitialized'   => ['onPageInitialized', 0]
         ]);
+    }
+
+    /**
+     * Add Blueprint logic for Admin
+     *
+     * @param Event $event
+     */
+    public function onBlueprintCreated(Event $event): void
+    {
+        $newtype = $event['type'];
+
+        if (strpos($newtype, 'modular/') === 0) {
+            return;
+        }
+
+        $blueprint = $event['blueprint'];
+        if ($blueprint->get('form/fields/tabs', null, '/')) {
+            $current_blueprint = new \Grav\Common\Data\Blueprint('plugin://seo-auto-tag/blueprints.yaml');
+            $blueprint->extend($current_blueprint, true);
+        }
+    }
+
+    /**
+     * Add our templates folder to the Twig template paths
+     */
+    public function onTwigTemplatePaths(): void
+    {
+        $this->grav['twig']->twig_paths[] = __DIR__ . '/templates';
+    }
+
+    /**
+     * Generate SEO tags
+     *
+     * @param Event $event
+     */
+    public function onPageInitialized(Event $event): void
+    {
+        $page = $this->grav['page'];
+        if (!$page || !$page->routable()) {
+            return;
+        }
+
+        $twig = $this->grav['twig'];
+
+        // Process the logic template and inject it into the page head using addInlineHtml as per requirement
+        $html = $twig->processTemplate('partials/seo-auto-tag.html.twig', ['page' => $page]);
+        $this->grav['assets']->addInlineHtml($html);
     }
 }
